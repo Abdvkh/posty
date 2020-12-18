d1f5403bae3c93c3742ca5d7ec44413ad8132563
abdvkh <e-address@bk.ru>
2020-12-18 16:12:58 +0500

    Add post (un-)like feature

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index()
     {
-        return view('posts.index');
+//        $posts = Post::get();// all[Laravel collection]
+        $posts = Post::paginate(20);// paginated[LengthAwarePaginator[Lar.Collection]]
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
     }
 
     public function store(Request $request)
