2d6d258524f618d53e6abf540d999773516e42fc
abdvkh <e-address@bk.ru>
2020-12-21 12:13:03 +0500

    Final tidying

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(['auth'])->only(['store', 'destroy']);
+    }
+
     public function index()
     {
 //        $posts = Post::get();// all[Laravel collection]
--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -2,7 +2,7 @@
 
 <div>
     <div class="mb-4">
-        <a href="{{ route('users.posts', auth()->user()) }}" class="font-bold">{{ $post->user->name }}</a>
+        <a href="{{ route('users.posts', $post->user) }}" class="font-bold">{{ $post->user->name }}</a>
 
         <span class="text-gray-600 text-sm">
                             {{ $post->created_at->diffForHumans() }}
